f32efafa218afc675586d9b4c8d0d86ca0c4da80
slatkisol <noa.zorko1@gmail.com>
2025-11-24 10:23:34 +0100

    dom i igra(1_100)

--- DOM/DOM/index.js
+++ DOM/DOM/index.js
@@ -1,3 +1,18 @@
 document.query
 
 alert("Radi sve");
+
+document.getElementById("title").style.color="red";
+
+document.getElementsByTagName("li")[0].textContent="Promijenjeno";
+
+document.getElementsByClassName("list").add("highlight");
+
+document.querySelector("a")[0].setAttribute("href", "https://bing.com");
+
+document.querySelectorAll("li").textContent="Stavka";
+
+document.getElementsByClassName("btn").element.classList.add("active");
+
+document.getElementsByClassName("list")[1].element.classList.remove("list");
+
--- guessdanumber/index2.js
+++ guessdanumber/index2.js
@@ -1,39 +1,57 @@
 
-
 function randNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-
-let Randomnumber=randNumber();
+let randomNumber = randNumber();
 let pokusaji = 0;
 
+let input = document.getElementById("guess");
+let msg = document.getElementById("msg");
+let attempts = document.getElementById("attempts");
 
-let input=document.getElementById("guess");
-let msg=document.getElementById("msg");
-let attempt = document.getElementById("attempts");
-
-function updateAttempts(){
-
-   attempts.innerHtml = "Pokušaji" + pokušaji;
-
+function updateAttempts() {
+  attempts.innerHTML = "Pokušaji: " + pokusaji;
 }
 updateAttempts();
 
-function checkNumber(event){
+function checkNumber(event) {
+  event.preventDefault();
 
-     event.preventDefault();
-     let value = parseInt(input.value);
-     console.log(value);
+  let value = parseInt(input.value);
 
+  if (isNaN(value) || value < 1 || value > 100) {
+    msg.innerHTML = "Unesi broj između 1 i 100";
+    msg.style.color = "crimson";
+    return;
+  }
+
+  pokusaji++;
+  updateAttempts();
+
+  if (value === randomNumber) {
+    msg.innerHTML = "Pogodak! Broj je " + randomNumber;
+    msg.style.color = "green";
+  } 
+  else if (value < randomNumber) {
+    msg.innerHTML = "Premalo!";
+    msg.style.color = "orange";
+  } 
+  else {
+    msg.innerHTML = "Previše!";
+    msg.style.color = "orange";
+  }
 }
-function newGame(){
 
+function newGame() {
+  randomNumber = randNumber();
+  pokusaji = 0;
+  input.value = "";
+  msg.innerHTML = "Nova igra";
+  msg.style.color = "black";
+  updateAttempts();
+  input.focus();
 }
-
-
-
-
 
 
 
